41f10f1f1ad64b8883502869fb668774d7144914
Marko675 <maki.marko.09@gmail.com>
2024-03-02 16:21:51 +0100

    The functionality to utilize the library as a plugin has been added

--- core/src/main/kotlin/org/holoeasy/HoloEasy.kt
+++ core/src/main/kotlin/org/holoeasy/HoloEasy.kt
@@ -1,31 +1,31 @@
 package org.holoeasy
 
-
 import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.java.JavaPlugin
 import org.holoeasy.pool.HologramPool
 import org.holoeasy.pool.IHologramPool
 import org.holoeasy.pool.InteractiveHologramPool
 
-object HoloEasy {
+class HoloEasy : JavaPlugin() {
 
-    @JvmField
-    var useLastSupportedVersion: Boolean = false
+    companion object {
+        @JvmField
+        var useLastSupportedVersion: Boolean = false
 
-    @JvmStatic
-    fun startPool(plugin: Plugin, spawnDistance: Double): IHologramPool {
-        val simplepool = HologramPool(plugin, spawnDistance)
-        return simplepool
+        @JvmStatic
+        fun startPool(plugin: Plugin, spawnDistance: Double): IHologramPool {
+            val simplepool = HologramPool(plugin, spawnDistance)
+            return simplepool
+        }
+
+        @JvmStatic
+        fun startInteractivePool(
+                plugin: Plugin, spawnDistance: Double,
+                minHitDistance: Float, maxHitDistance: Float
+        ): IHologramPool {
+            val simplepool = HologramPool(plugin, spawnDistance)
+            val interactivepool = InteractiveHologramPool(simplepool, minHitDistance, maxHitDistance)
+            return interactivepool
+        }
     }
-
-    @JvmStatic
-    fun startInteractivePool(
-        plugin: Plugin, spawnDistance: Double,
-        minHitDistance: Float, maxHitDistance: Float
-    ): IHologramPool {
-        val simplepool = HologramPool(plugin, spawnDistance)
-        val interactivepool = InteractiveHologramPool(simplepool, minHitDistance, maxHitDistance)
-        return interactivepool
-    }
-
-
 }
